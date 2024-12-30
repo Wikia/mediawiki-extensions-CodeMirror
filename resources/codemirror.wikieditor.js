74fc1fa08da201676fef0764e835f2cb9e878720
Mateusz Iwaniszczuk <miwaniszczuk@fandom.com>
2024-12-30 16:12:45 +0200

    fix(PLATFORM-9834): fix icon

--- resources/codemirror.wikieditor.js
+++ resources/codemirror.wikieditor.js
@@ -167,7 +167,7 @@ class CodeMirrorWikiEditor extends CodeMirror {
 							CodeMirrorPreferences: {
 								type: 'toggle',
 								label: mw.msg( 'codemirror-prefs-title' ),
-								oouiIcon: 'settings',
+								oouiIcon: 'highlight',
 								action: {
 									type: 'callback',
 									execute: () => {
