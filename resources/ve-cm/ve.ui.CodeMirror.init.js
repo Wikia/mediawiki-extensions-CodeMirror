ccfb95a9c57a38113f3770eada81bbaa37b3b172
tmusial99 <tmusial@fandom.com>
2025-01-30 15:55:14 +0100

    NO-TASK | Fix VE init script to ensure CM is loaded before VE activation

--- resources/ve-cm/ve.ui.CodeMirror.init.js
+++ resources/ve-cm/ve.ui.CodeMirror.init.js
@@ -1,9 +1,18 @@
-require( '../ext.CodeMirror.data.js' );
+if ( mw.config.get( 'skin' ) !== 'fandommobile' ) {
+	require( '../ext.CodeMirror.data.js' );
 
-const urlParams = new URLSearchParams( window.location.search );
+	const urlParams = new URLSearchParams( window.location.search );
 
+<<<<<<< Updated upstream
 if ( mw.config.get( 'extCodeMirrorConfig' ).useV6 || urlParams.get( 'cm6enable' ) ) {
 	mw.loader.load( 'ext.CodeMirror.v6.visualEditor' );
 } else {
 	mw.loader.load( 'ext.CodeMirror.visualEditor' );
+=======
+	if ( mw.config.get( 'extCodeMirrorConfig' ).useV6 || urlParams.get( 'cm6enable' ) ) {
+		mw.libs.ve.targetLoader.addPlugin( () => mw.loader.using( 'ext.CodeMirror.v6.visualEditor' ) );
+	} else {
+		mw.libs.ve.targetLoader.addPlugin( () => mw.loader.using( 'ext.CodeMirror.visualEditor' ) );
+	}
+>>>>>>> Stashed changes
 }
